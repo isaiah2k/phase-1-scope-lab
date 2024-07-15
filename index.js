dd2c37d27d9d9427e1569c459d1cfd91563fbb4d
Isaiah <zaymgmt707@gmail.com>
2024-07-15 14:02:00 -0700

    updates index.js

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
-var customerName = `bob`
+// Variable to store the name of the customer, bob
+var customerName = 'bob'
+// Function to convert the customer name to uppercase
 function upperCaseCustomerName() {
+    // Converts the customerName to uppercase and updates the variable
     customerName = customerName.toUpperCase()
 }
+// Function to set the best customer
 function setBestCustomer() {
-    bestCustomer = `not bob` 
+    // Sets the bestCustomer variable to 'not bob'
+    bestCustomer = 'not bob' 
 }
+// Function to overwrite the best customer
 function overwriteBestCustomer() {
-    bestCustomer = `maybe bob`
+    // Changes the bestCustomer variable to 'maybe bob'
+    bestCustomer = 'maybe bob'
 }
-const LeastFavoriteCustomer = `definitely bob`
+// Constant variable to store the least favorite customer
+const leastFavoriteCustomer = 'definitely bob'
+// Function to change the least favorite customer
 function changeLeastFavoriteCustomer() {
-    LeastFavoriteCustomer = `bob`
+    // Unsuccessfully changes the leastFavoriteCustomer variable, which will cause an error because leastFavoriteCustomer is a constant
+    leastFavoriteCustomer = 'bob'
 }
